dfa9e10ec4e6f2d63b6698a844a3d2c6cdb913e2
adugu001 <adugu001@odu.edu>
2026-02-25 20:24:44 -0500

    Need to get the build system to run, but some scaffolding put in place

---
+++ src/main/java/edu/odu/cs/cs350/codeComp.java
@@ -0,0 +1,8 @@
+package edu.odu.cs.cs350;
+public class codeComp {
+
+    public static void main(String[] args) {
+        // The classic greeting
+        System.out.println("Hello, World!");
+    }
+}
