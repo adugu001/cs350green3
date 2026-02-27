ccf9c8ad028db04ab7406a13c822fb3d4568ea33
adugu001 <adugu001@odu.edu>
2026-02-27 17:42:32 -0500

    hello world! Done w gradle. './gradlew run' to run

--- src/main/java/edu/odu/cs/cs350/CodeComp.java
+++ src/main/java/edu/odu/cs/cs350/CodeComp.java
@@ -1,8 +1,8 @@
 package edu.odu.cs.cs350;
-public class codeComp {
+public class CodeComp {
 
     public static void main(String[] args) {
-        // The classic greeting
         System.out.println("Hello, World!");
+        
     }
 }
